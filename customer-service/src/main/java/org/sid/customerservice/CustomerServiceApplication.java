a39450095ae9236c0646866f4f123d6ad4602435
ahasla <y.ahasla@itroadgroup.com>
2024-02-16 15:58:25 +0100

    change gateway service dependencies

--- customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,13 @@
 package org.sid.customerservice;
 
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -10,4 +16,16 @@ public class CustomerServiceApplication {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository){
+		return args -> {
+			customerRepository.saveAll(
+					List.of(
+							Customer.builder().name("soso").email("youness@gmail.com").build(),
+							Customer.builder().name("nono").email("youness@gmail.com").build()
+					)
+			);
+			customerRepository.findAll().forEach(System.out::println);
+		};
+	}
 }
